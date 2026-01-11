8f7b0777cb5392b4c73ab5e86a5ae52703014536
dean <dingnan@nrec.com>
2026-01-11 14:31:48 +0800

    Add initial implementation of guessing game with random number generation

---
+++ guessing_game/src/main.rs
@@ -0,0 +1,34 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
+fn main() {
+    println!("Guess the number!");
+    let secret_number = rand::rng().random_range(1..=100);
+    println!("Please input your guess.");
+
+    loop {
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+        println!("You guessed: {guess}");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please enter a valid number!");
+                continue;
+            }
+        };
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
+}
